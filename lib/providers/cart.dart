328a26535660355df22541167ee08768d21bcc10
shreyassingh431 <30365246+shreyassingh431@users.noreply.github.com>
2020-11-29 19:44:31 +0530

    Created cart with update or add Items

---
+++ lib/providers/cart.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/foundation.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+
+  CartItem({@required this.id,
+    @required this.title,
+    @required this.quantity,
+    @required this.price })
+
+}
+
+class Cart with ChangeNotifier {
+
+  Map<String, CartItem> _items;
+
+  Map<String, CartItem> get items {
+    return {...items};
+  }
+
+  void addItem(String productId, double price, String title) {
+    if (_items.containsKey(productId)) {
+      //update quantity
+      _items.update(productId, (existngCartItem) =>
+          CartItem(id: existngCartItem.id,
+              title: existngCartItem.title,
+              quantity: existngCartItem + 1,
+              price: existngCartItem.price));
+    } else {
+      _items.putIfAbsent(productId, () =>
+          CartItem(id: DateTime.now().toString(),
+              title: title,
+              price: price,
+              quantity: 1));
+    }
+  }
+
+}
